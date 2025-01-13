205ebc9b29c1fa742b3a6d518580be48da140f76
IbnuTopanAdib <resyaderesya@gmail.com>
2025-01-13 12:06:51 +0700

    add quiz to the screen

--- quiz/src/components/Quiz.jsx
+++ quiz/src/components/Quiz.jsx
@@ -1,11 +1,33 @@
 import { useState } from 'react'
+import QUESTION from '../question.js';
 
 const Quiz = () => {
 
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
     const [userAnswer, setUserAnswer] = useState([]);
+    const activeQuestionIndex = userAnswer.length;
+
+    const handleUserAnswer = (answer) => {
+        setUserAnswer((prevAnswer) => {
+            return [...prevAnswer, answer]
+        })
+    }
+
+
     return (
-        <div>Quiz</div>
+        <div id='quiz'>
+            <div id="question">
+                <h2>{ QUESTION[activeQuestionIndex].text }</h2>
+            </div>
+            <ul id='answers'>
+                {
+                    QUESTION[activeQuestionIndex].answers.map((answer) => (
+                        <li className="answer">
+                            <button onClick={handleUserAnswer}>{ answer }</button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     )
 }
 
