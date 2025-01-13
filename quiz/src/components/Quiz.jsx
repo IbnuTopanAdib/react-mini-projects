fdd2a57452127daf3a6e12fc44bf57c2f4636a1f
IbnuTopanAdib <resyaderesya@gmail.com>
2025-01-13 12:18:14 +0700

    fix last scene and shuffle answers

--- quiz/src/components/Quiz.jsx
+++ quiz/src/components/Quiz.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import QUESTION from '../question.js';
+import quizCompleteImg from '../assets/quiz-complete.png'
 
 const Quiz = () => {
 
     const [userAnswer, setUserAnswer] = useState([]);
     const activeQuestionIndex = userAnswer.length;
+
+    const isQuizCompleted = activeQuestionIndex === QUESTION.length;
 
     const handleUserAnswer = (answer) => {
         setUserAnswer((prevAnswer) => {
@@ -12,17 +15,29 @@ const Quiz = () => {
         })
     }
 
+    if (isQuizCompleted) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswer = [...QUESTION[activeQuestionIndex].answers];
+    shuffledAnswer.sort(() => Math.random() - 0.5)
+
 
     return (
         <div id='quiz'>
             <div id="question">
-                <h2>{ QUESTION[activeQuestionIndex].text }</h2>
+                <h2>{QUESTION[activeQuestionIndex].text}</h2>
             </div>
             <ul id='answers'>
                 {
-                    QUESTION[activeQuestionIndex].answers.map((answer) => (
+                   shuffledAnswer.map((answer) => (
                         <li className="answer">
-                            <button onClick={handleUserAnswer}>{ answer }</button>
+                            <button onClick={handleUserAnswer}>{answer}</button>
                         </li>
                     ))
                 }
